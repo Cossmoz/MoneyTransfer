57bc4871b61b3c68bd1041359f14a63e4942c032
Constantin Zamotaev <cossx@yahoo.com>
2021-01-15 20:13:10 +0400

    txt added

--- src/MoneyTransfer/MoneyTransfer.java
+++ src/MoneyTransfer/MoneyTransfer.java
@@ -5,6 +5,6 @@ public class MoneyTransfer {
         int  balance = 2_000_000_000;
         int transfer = 500_000_000;
         int total_balance = balance + transfer;
-        System.out.println(total_balance);
+        System.out.println("Баланс счета = " + total_balance);
     }
 }
