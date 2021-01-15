39215cd43808e6d1500664fb566899e4984c8b9f
Constantin Zamotaev <cossx@yahoo.com>
2021-01-15 20:13:10 +0400

    Project added

---
+++ src/MoneyTransfer/MoneyTransfer.java
@@ -0,0 +1,10 @@
+package MoneyTransfer;
+
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int  balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total_balance = balance + transfer;
+        System.out.println(total_balance);
+    }
+}
